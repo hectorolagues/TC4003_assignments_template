c0da0b36f5ccefca06823ad21b1d59e2f8a976e4
Hector Olagues <Hector.Olagues@continental-corporation.com>
2020-09-01 18:38:11 -0500

    HeOl: Assignment 1-1 is done.

--- assignment1-1/q2.go
+++ assignment1-1/q2.go
@@ -1,8 +1,18 @@
+//********************************************************
+//********************************************************
+//*** Name: Hector Gabriel Olagues Torres
+//*** UserID: hectorolagues
+//*** Assignment 1-1, Q2 - Parallel sum: intended to sum a
+//*** list of numbers in a file in parallel.
+//********************************************************
+//********************************************************
+
 package cos418_hw1_1
 
 import (
 	"bufio"
 	"io"
+	"os"
 	"strconv"
 )
 
@@ -10,8 +20,11 @@ import (
 // You should only output to `out` once.
 // Do NOT modify function signature.
 func sumWorker(nums chan int, out chan int) {
-	// TODO: implement me
-	// HINT: use for loop over `nums`
+	total := 0
+	for number := range nums {
+		total += number
+	}
+	out <- total
 }
 
 // Read integers from the file `fileName` and return sum of all values.
@@ -20,10 +33,41 @@ func sumWorker(nums chan int, out chan int) {
 // You should use `checkError` to handle potential errors.
 // Do NOT modify function signature.
 func sum(num int, fileName string) int {
-	// TODO: implement me
-	// HINT: use `readInts` and `sumWorkers`
-	// HINT: used buffered channels for splitting numbers between workers
-	return 0
+	// Open the file stored in fileName and check for errors.
+	file, err := os.Open(fileName)
+	checkError(err)
+	// Close the file when returning from this function, by using keyword defer.
+	defer file.Close()
+
+	integers, err := readInts(file)
+	checkError(err)
+
+	// Create buffered channels
+	// The length of numbers is divided by the number of go routines or workers in chNums, in order to split numbers between workers.
+	// chOut has a buffer length equal to the number of go routines or workers.
+	chNums := make(chan int, len(integers)/num)
+	chOut := make(chan int, num)
+
+	// Sum the values concurrently with go routines
+	for i := 0; i < num; i++ {
+		go sumWorker(chNums, chOut)
+	}
+
+	// Send values from integers list to channel chNums
+	for _, val := range integers {
+		chNums <- val
+	}
+	close(chNums)
+
+	// Total receives from channel chOut the sum calculated in go routines
+	total := 0
+	for i := 0; i < num; i++ {
+		total += <-chOut
+	}
+	close(chOut)
+
+	// Returns the total amount calculated concurrently in sumWorker
+	return total
 }
 
 // Read a list of integers separated by whitespace from `r`.
